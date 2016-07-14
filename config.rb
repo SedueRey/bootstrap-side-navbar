4d2e48fedeeb43b74e04bcb7eede1960c69dc529
Eduardo Rey <proyectos@seduerey.com>
2016-07-14 11:52:12 +0200

    Antarctica adaptation

--- config.rb
+++ config.rb
@@ -1,94 +1,19 @@
-###
-# Blog settings
-###
+require 'compass/import-once/activate'
+# require 'bootstrap-sass'
+# Require any additional compass plugins here.
 
-Time.zone = "UTC"
+# Set this to the root of your project when deployed:
+http_path = "/"
 
-###
-# Compass
-###
+css_dir  = "source/assets/stylesheets"
+sass_dir = "source/assets/stylesheets"
 
-set :haml, { format: :html5, attr_wrapper: '"' }
-set :markdown, { smartypants: true }
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+output_style = :compressed
 
-# Susy grids in Compass
-# First: gem install susy
-# require 'susy'
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = true
 
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
-
-###
-# Page options, layouts, aliases and proxies
-###
-
-# Per-page layout changes:
-#
-# With no layout
-# page "/path/to/file.html", :layout => false
-#
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
-
-# Proxy (fake) files
-# page "/this-page-has-no-template.html", :proxy => "/template-file.html" do
-#   @which_fake_page = "Rendering a fake page with a variable"
-# end
-
-###
-# Helpers
-###
-
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
-
-set :css_dir, 'assets/stylesheets'
-set :js_dir, 'assets/javascripts'
-set :images_dir, 'assets/images'
-set :partials_dir, 'partials'
-
-set :relative_links, true
-
-# Build-specific configuration
-configure :build do
-  # For example, change the Compass output style for deployment
-  activate :minify_css
-
-  # Minify Javascript on build
-  activate :minify_javascript
-
-  # Enable cache buster
-  # activate :cache_buster
-
-  # Use relative URLs
-  activate :relative_assets
-
-  # Compress PNGs after build
-  # First: gem install middleman-smusher
-  # require "middleman-smusher"
-  # activate :smusher
-
-  # Or use a different image path
-  # set :http_path, "/Content/images/"
-end
-
-activate :deploy do |deploy|
-  deploy.build_before = true
-  deploy.method = :git
-  deploy.remote = "origin"
-  deploy.branch = "gh-pages"
-end
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
